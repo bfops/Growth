b5fadcb5bcb5664c09737886953e9f6c4002065d
Ben Foppa <benjamin.foppa@gmail.com>
2014-06-26 08:14:51 -0400

    Removed scripts folder

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cabal build $@ 2>&1 | grep -v "^\(Loading package\|You are using a new version of LLVM that hasn't been tested yet!$\|We will try though...$\)"
-exit $PIPESTATUS
--- scripts/docgen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Haddock coverage:"
-cabal haddock --executables 2>&1 | grep "^ [ 0-9][ 0-9][0-9]%"
-exit $PIPESTATUS
